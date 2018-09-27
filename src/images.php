905b9c37a7954bcc9be44694bd5675ba7386034f
qinuoyun <qinuoyun@qq.com>
2018-09-27 13:53:14 +0800

    初始化

---
+++ src/images.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * this7 PHP Framework
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2016-2018 Yan TianZeng<qinuoyun@qq.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.ub-7.com
+ */
+namespace this7\images;
+class images {
+
+    public function demo($value = '') {
+        echo "demo";
+    }
+}
